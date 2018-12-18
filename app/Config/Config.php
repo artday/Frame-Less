4494f3896d61db31da42ebc7ec3e79834bfa78ef
artday <admin@artday.com.ua>
2018-12-18 05:25:46 +0200

    added load() method for loading from loaders

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -2,7 +2,21 @@
 
 namespace App\Config;
 
+use App\Config\Loaders\Loader;
+
 class Config
 {
+    protected $config = [];
+
+    public function load(array $loaders)
+    {
+        foreach ($loaders as $loader) {
+            if (!$loader instanceof Loader) {
+                continue;
+            }
+            $this->config = array_merge($this->config, $loader->parse());
+        }
+        return $this;
+    }
 
 }

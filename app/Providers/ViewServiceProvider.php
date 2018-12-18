50b9cd97fb386205223137f1f54bd42f35dcc72e
artday <admin@artday.com.ua>
2018-12-18 06:07:23 +0200

    added View service provider, which shares View class extend Twig with debug extension and Routes Path extension

---
+++ app/Providers/ViewServiceProvider.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Providers;
+
+use App\Views\Extensions\PathExtension;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+use App\Views\View;
+use League\Route\RouteCollection;
+use Twig_Environment;
+use Twig_Extension_Debug;
+use Twig_Loader_Filesystem;
+
+class ViewServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $provides = [
+        View::class
+    ];
+
+    /**
+     * Use the register method to register items with the container via the
+     * protected $this->container property or the `getContainer` method
+     * from the ContainerAwareTrait.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $container = $this->getContainer();
+
+        $config = $container->get('config');
+        $route = $container->get(RouteCollection::class);
+
+        $container->share(View::class, function() use ($config, $route) {
+            $loader = new Twig_Loader_Filesystem(base_path('views'));
+
+            $twig = new Twig_Environment($loader, [
+                'cache' => $config->get('cache.views.path'),
+                'debug' => $config->get('app.debug')
+            ]);
+
+            if ($config->get('app.debug')) {
+                $twig->addExtension(new Twig_Extension_Debug);
+            }
+
+            $twig->addExtension(new PathExtension($route));
+
+            return new View($twig);
+        });
+
+    }
+}

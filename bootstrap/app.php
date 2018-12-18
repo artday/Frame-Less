536588b3c4b4e736936630b4c9233ae02c4ddff9
artday <admin@artday.com.ua>
2018-12-18 02:53:50 +0200

    load enviroment variables

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,7 +1,13 @@
 <?php
 
+use Dotenv\Exception\InvalidPathException;
+
 session_start();
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-var_dump('a');
+try {
+    $dotenv = (new \Dotenv\Dotenv(base_path()))->load();
+} catch (InvalidPathException $e) {
+    echo "<br>There is no ENV  were find<br>";
+}

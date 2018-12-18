136f4de47d7a303e397b5d78a3b1ba25b97a44da
artday <admin@artday.com.ua>
2018-12-18 06:17:34 +0200

    added app config

---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'name' => env('APP_NAME'),
+    'debug' => env('APP_DEBUG', false),
+
+    'providers' => [
+        'App\Providers\AppServiceProvider',
+        'App\Providers\ViewServiceProvider',
+    ],
+];

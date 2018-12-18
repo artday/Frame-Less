d57c1041d499eb26cd2b6498321598cbdf52c43f
artday <admin@artday.com.ua>
2018-12-18 04:54:40 +0200

    updated added autowiring with delegating to reflection container add providers to container

--- bootstrap/container.php
+++ bootstrap/container.php
@@ -1,5 +1,13 @@
 <?php
 
 use League\Container\Container;
+use League\Container\ReflectionContainer;
+
+use App\Providers\AppServiceProvider;
+use App\Providers\ViewServiceProvider;
 
 $container = new Container;
+$container->delegate(new ReflectionContainer);
+
+$container->addServiceProvider(AppServiceProvider::class);
+$container->addServiceProvider(ViewServiceProvider::class);

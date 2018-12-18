5f20ea397a68e3da53fb07879b82cb9f5d9842c1
artday <admin@artday.com.ua>
2018-12-18 06:16:57 +0200

    added get() method added caching system for config

--- app/Config/Config.php
+++ app/Config/Config.php
@@ -7,6 +7,7 @@ use App\Config\Loaders\Loader;
 class Config
 {
     protected $config = [];
+    protected $cache = [];
 
     public function load(array $loaders)
     {
@@ -17,6 +18,48 @@ class Config
             $this->config = array_merge($this->config, $loader->parse());
         }
         return $this;
+    }
+
+    public function get($key, $default = null)
+    {
+        if ($this->existsInCache($key)) {
+            return $this->fromCache($key);
+        }
+
+        $value = $this->extractFromConfig($key);
+
+        return $value
+            ? $this->addToCache($key, $value)
+            : $this->addToCache($key, $default);
+    }
+
+    protected function existsInCache($key)
+    {
+        return isset($this->cache[$key]);
+    }
+
+    protected function fromCache($key)
+    {
+        return $this->cache[$key];
+    }
+
+    protected function extractFromConfig($key)
+    {
+        $filtered = $this->config;
+        foreach (explode('.', $key) as $segment) {
+            if (array_key_exists($segment, $filtered)) {
+                $filtered = $filtered[$segment];
+                continue;
+            }
+            return;
+        }
+        return $filtered;
+    }
+
+    protected function addToCache($key, $value)
+    {
+        $this->cache[$key] = $value;
+        return $value;
     }
 
 }

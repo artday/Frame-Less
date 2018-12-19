b2005394edc26deb23e46ea605a8eebfb31a730c
artday <admin@artday.com.ua>
2018-12-19 03:35:41 +0200

    added provider to share data in view

---
+++ app/Providers/ViewShareServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+namespace App\Providers;
+
+use App\Auth\Auth;
+use App\Views\View;
+use App\Session\Flash;
+use App\Security\Csrf;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+
+class ViewShareServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+    /**
+     * @var array
+     */
+    protected $provides = [
+        //
+    ];
+
+    /**
+     * Use the register method to register items with the container via the
+     * protected $this->container property or the `getContainer` method
+     * from the ContainerAwareTrait.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Method will be invoked on registration of a service provider implementing
+     * this interface. Provides ability for eager loading of Service Providers.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $container = $this->getContainer();
+
+        $container->get(View::class)->share([
+            'config' => $container->get('config'),
+            'auth' => $container->get(Auth::class),
+            'flash' => $container->get(Flash::class),
+            'csrf' => $container->get(Csrf::class),
+        ]);
+    }
+}

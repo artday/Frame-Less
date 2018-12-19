f99524a2a070870b9483b94fc98d344e70c38b42
artday <admin@artday.com.ua>
2018-12-19 03:52:02 +0200

    added provider for hashing, which share Hasher class  in container

---
+++ app/Providers/HashServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Providers;
+
+use App\Auth\Hashing\BcryptHasher;
+use App\Auth\Hashing\Hasher;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class HashServiceProvider extends AbstractServiceProvider
+{
+    /**
+     * @var array
+     */
+    protected $provides = [
+        Hasher::class
+    ];
+
+    /**
+     * Use the register method to register items with the container via the
+     * protected $this->container property or the `getContainer` method
+     * from the ContainerAwareTrait.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->getContainer()->share(Hasher::class, function () {
+            return new BcryptHasher();
+        });
+    }
+}

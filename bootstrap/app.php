62e6ca05031b8610146708c6fade116bd66fd859
artday <admin@artday.com.ua>
2018-12-19 06:32:32 +0200

    Dispatching routes, handle Errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,9 @@
 <?php
 
 use Dotenv\Dotenv;
+use App\Views\View;
+use App\Exceptions\Handler;
+use App\Session\SessionStore;
 use League\Route\RouteCollection;
 use Dotenv\Exception\InvalidPathException;
 
@@ -18,8 +21,21 @@ require_once base_path('bootstrap/container.php');
 
 $route = $container->get(RouteCollection::class);
 
+require_once base_path('bootstrap/middleware.php');
+
 require_once base_path('routes/web.php');
 
-$response = $route->dispatch(
-    $container->get('request'), $container->get('response')
-);
+try {
+    $response = $route->dispatch(
+        $container->get('request'), $container->get('response')
+    );
+
+} catch (Exception $e) {
+    $handler = new Handler(
+        $e,
+        $container->get(SessionStore::class),
+        $container->get('response'),
+        $container->get(View::class)
+    );
+    $response = $handler->respond();
+}

269aa2a961d9a19609f058d0985c234a80f7e85d
artday <admin@artday.com.ua>
2018-12-19 06:47:00 +0200

    added authenticated middleware for checking if user is authenticated

---
+++ app/Middleware/Authenticated.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Middleware;
+
+use App\Auth\Auth;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class Authenticated
+{
+    protected $auth;
+
+    public function __construct(Auth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next)
+    {
+        if (!$this->auth->check()) {
+            $response = redirect('/');
+        }
+        return $next($request, $response);
+    }
+}

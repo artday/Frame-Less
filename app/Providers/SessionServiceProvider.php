5e371ac6261e470339aa6d1ed512f0a5c2469f5b
artday <admin@artday.com.ua>
2018-12-19 03:46:24 +0200

    added Session provider which just shares Session class in container

---
+++ app/Providers/SessionServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Providers;
+
+use App\Session\Session;
+use App\Session\SessionStore;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+
+class SessionServiceProvider extends AbstractServiceProvider
+{
+    protected $provides = [
+        SessionStore::class
+    ];
+
+    /**
+     * Use the register method to register items with the container via the
+     * protected $this->container property or the `getContainer` method
+     * from the ContainerAwareTrait.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $container = $this->getContainer();
+
+        $container->share(SessionStore::class, function () {
+            return new Session();
+        });
+    }
+}

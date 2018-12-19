db4f55f74a2a77b469c0126cf2e5575b025d37af
artday <admin@artday.com.ua>
2018-12-19 07:00:30 +0200

    custom exceptions handler

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use App\Views\View;
+use ReflectionClass;
+use App\Session\SessionStore;
+use Psr\Http\Message\ResponseInterface;
+
+class Handler
+{
+    protected $exception;
+    protected $session;
+    protected $view;
+    protected $response;
+
+    public function __construct(
+        Exception $exception,
+        SessionStore $session,
+        ResponseInterface $response,
+        View $view
+    ) {
+        $this->exception = $exception;
+        $this->session = $session;
+        $this->response = $response;
+        $this->view = $view;
+    }
+
+    public function respond()
+    {
+        $class = (new ReflectionClass($this->exception))->getShortName();
+
+        if (method_exists($this, $method = "handle{$class}")) {
+            return $this->{$method}($this->exception);
+        }
+
+        return $this->unhandleException($this->exception);
+    }
+
+    protected function handleValidationException(ValidationException $e)
+    {
+        $this->session->set([
+            'errors' => $e->getErrors(),
+            'old' => $e->getOldInput()
+        ]);
+        return redirect($e->getPath());
+    }
+
+    protected function handleCsrfTokenException(CsrfTokenException $e)
+    {
+        return $this->view->render($this->response, 'errors/csrf.twig');
+    }
+
+    protected function unhandleException(Exception $e)
+    {
+        throw $e;
+    }
+
+}

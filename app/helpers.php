4abfde4246ab342b4c8c490b79639e97c1adb910
artday <admin@artday.com.ua>
2018-12-19 06:37:46 +0200

    added redirect() and random_string() helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,24 @@
 <?php
 
+use Zend\Diactoros\Response\RedirectResponse;
+
+if (!function_exists('redirect')) {
+    function redirect($path) {
+        return new RedirectResponse($path);
+    }
+}
+
+if (!function_exists('random_string')) {
+    function random_string($length = 32) {
+        $rand = '';
+        while ($length) {
+            $rand .= rand(0, 9);
+            $length--;
+        }
+        return bin2hex(base64_encode($rand));
+    }
+}
+
 if (!function_exists('base_path')) {
     function base_path($path = '') {
         return __DIR__ . '/..//' . ($path ? DIRECTORY_SEPARATOR . $path : $path);

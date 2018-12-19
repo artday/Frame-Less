40fcbe658eb2eed7ede8939b08d8923382f4118d
artday <admin@artday.com.ua>
2018-12-19 06:36:34 +0200

    added database connection options

--- config/db.php
+++ config/db.php
@@ -2,11 +2,14 @@
 
 return [
     'mysql' => [
-        'driver' => env('DB_DRIVER', 'pdo_mysql'),
+        'driver' => env('DB_DRIVER', 'mysql'),
         'host' => env('DB_HOST', '127.0.0.1'),
         'port' => env('DB_PORT', '3306'),
-        'dbname' => env('DB_DATABASE', 'frameless'),
-        'user' => env('DB_USERNAME', 'root'),
+        'database' => env('DB_DATABASE', 'database'),
+        'username' => env('DB_USERNAME', 'root'),
         'password' => env('DB_PASSWORD', false),
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci',
+        'prefix' => env('DB_PREFIX', ''),
     ]
 ];

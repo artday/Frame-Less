b7b376eae2c3d1442bd5620809400f93f38ba303
artday <admin@artday.com.ua>
2018-12-19 06:30:23 +0200

    register Services and Middlewares

--- config/app.php
+++ config/app.php
@@ -7,5 +7,22 @@ return [
     'providers' => [
         'App\Providers\AppServiceProvider',
         'App\Providers\ViewServiceProvider',
+        'App\Providers\DatabaseServiceProvider',
+        'App\Providers\SessionServiceProvider',
+        'App\Providers\HashServiceProvider',
+        'App\Providers\AuthServiceProvider',
+        'App\Providers\FlashServiceProvider',
+        'App\Providers\CsrfServiceProvider',
+        'App\Providers\ValidationServiceProvider',
+        'App\Providers\CookieServiceProvider',
+        'App\Providers\ViewShareServiceProvider',
     ],
+
+    'middleware' => [
+        'App\Middleware\ShareValidationErrors',
+        'App\Middleware\ClearValidationErrors',
+        'App\Middleware\Authenticate',
+        'App\Middleware\AuthenticateFromCookie',
+        'App\Middleware\CsrfGuard',
+    ]
 ];

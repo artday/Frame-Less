47379aa920880c0bc16dae4cb498454cbd2bcbc8
artday <admin@artday.com.ua>
2018-12-19 07:18:58 +0200

    boot Database ORM with connection

---
+++ app/Providers/DatabaseServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Database\Capsule\Manager;
+use League\Container\ServiceProvider\AbstractServiceProvider;
+use League\Container\ServiceProvider\BootableServiceProviderInterface;
+
+class DatabaseServiceProvider extends AbstractServiceProvider implements BootableServiceProviderInterface
+{
+    protected $provides = [];
+
+    public function register()
+    {
+    }
+
+    public function boot()
+    {
+        $container = $this->getContainer();
+        $config = $container->get('config');
+
+        $capsule = new Manager();
+        $capsule->addConnection($config->get('db.' . env('DB_TYPE')));
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+    }
+}

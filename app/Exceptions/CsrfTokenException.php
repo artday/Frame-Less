588d7d810e5c83a419883b755ad203926075c2dc
artday <admin@artday.com.ua>
2018-12-19 07:03:41 +0200

    added custom exceptions

---
+++ app/Exceptions/CsrfTokenException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class CsrfTokenException extends Exception
+{
+//    protected $request;
+//    protected $errors;
+
+    public function __construct()
+    {
+
+    }
+
+}

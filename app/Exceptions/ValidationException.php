0a5c0e418238557541179d5a1e04145c8e785e4e
artday <admin@artday.com.ua>
2019-04-07 15:31:11 +0300

    refactor

--- app/Exceptions/ValidationException.php
+++ app/Exceptions/ValidationException.php
@@ -3,14 +3,14 @@
 namespace App\Exceptions;
 
 use Exception;
-use Psr\Http\Message\ServerRequestInterface;
+use App\Core\Framework\Request\ServerRequest;
 
 class ValidationException extends Exception
 {
     protected $request;
     protected $errors;
 
-    public function __construct(ServerRequestInterface $request, array $errors)
+    public function __construct(ServerRequest $request, array $errors)
     {
         $this->request = $request;
         $this->errors = $errors;
@@ -18,7 +18,8 @@ class ValidationException extends Exception
 
     public function getPath()
     {
-        return $this->request->getUri()->getPath();
+//        return $this->request->getUri()->getPath();
+        return $this->request->getReferer();
     }
 
     public function getOldInput()
